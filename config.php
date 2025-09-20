9c24e7f3336b1bb815780783b6cd2be7b7083ec3
Larissa Lima <la.lima2103@gmail.com>
2025-09-20 13:37:16 -0300

    primeiro commit

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = 'Killian!#0';
+    $dbName = 'novo_cadastro';
+
+
+    $conexao = new mysqli ($dbHost, dbUsername, dbPassword, dbName)
+
+
+   // if ($conexao->connect_errno)
+   // {
+   //     echo "Erro";
+   // }
+
+   // else
+   // {
+   //     echo "Conexão efetuada com sucesso"
+
+   // }
+
+
+
+?>
+
